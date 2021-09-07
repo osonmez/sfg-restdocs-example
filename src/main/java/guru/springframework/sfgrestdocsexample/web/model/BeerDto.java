c2734a9c2a782e24e9232164599cc1c3ac253066
Omer Sonmez <omer.sonmez@nokia.com>
2021-09-07 13:36:27 +0300

    rest docs added

--- src/main/java/guru/springframework/sfgrestdocsexample/web/model/BeerDto.java
+++ src/main/java/guru/springframework/sfgrestdocsexample/web/model/BeerDto.java
@@ -9,6 +9,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -36,6 +38,7 @@ public class BeerDto {
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
+    @Size(min = 3, max = 100)
     private String beerName;
 
     @NotNull
@@ -49,6 +52,7 @@ public class BeerDto {
     @NotNull
     private BigDecimal price;
 
+    @Positive
     private Integer quantityOnHand;
 
 }
